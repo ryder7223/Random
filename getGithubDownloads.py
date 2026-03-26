cc739aa3d6541f074dae4487525857b766a5cb3d
Ryder7223 <114200735+ryder7223@users.noreply.github.com>
2026-03-26 15:08:10 +0800

    Update getGithubDownloads.py

--- getGithubDownloads.py
+++ getGithubDownloads.py
@@ -1,23 +1,40 @@
 import requests
 import json
+import pprint
 
-user = "ryder7223"
-repo = "Auto-Practice"
-fileName = "ryder7223.autopractice.geode"
+user = "werwolv"
+repo = "imhex"
 
 url = f"https://api.github.com/repos/{user}/{repo}/releases"
 response = requests.get(url)
 jsonurl = json.loads(response.text)
-downloadCount = "None"
-totalDownloadCount = 0
+totalDownloads = 0
+extendedPrint = False
+
+downloadDict = {}
 
 for i in jsonurl:
+	releaseName = i["name"]
 	for j in i["assets"]:
-		if j["name"] == fileName:
+		if extendedPrint:
 			print(f"Release Name: {i["name"]}")
 			print(f"Filename: {j["name"]}")
 			print(f"Downloads: {j["download_count"]}\n")
-			totalDownloadCount += int(j["download_count"])
 
-print(f"Total Downloads: {totalDownloadCount}")
+		if j["name"] not in downloadDict:
+			downloadDict[j["name"]] = {
+				"releases": {},
+				"total": 0
+			}
+
+		downloadDict[j["name"]]["releases"][i["name"]] = int(j["download_count"])
+		downloadDict[j["name"]]["total"] += int(j["download_count"])
+		totalDownloads += int(j["download_count"])
+
+print("Downloads:")
+maxLen = max(len(i) for i in downloadDict)
+for i in downloadDict:
+	print(f"{i.ljust(maxLen)}  {downloadDict[i]['total']:>10}")
+
+print(f"\nTotal Downloads: {totalDownloads}")
 input()
