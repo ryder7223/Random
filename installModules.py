76c9c3489693ba32d771c99c014aab3e8e55005d
Ryder7223 <114200735+ryder7223@users.noreply.github.com>
2026-06-17 08:12:12 +0800

    Update installModules.py

--- installModules.py
+++ installModules.py
@@ -3,20 +3,36 @@ import importlib
 import sys
 
 requiredModules = {
-    "requests": "requests"
+    "requests": {
+        "package": "requests"
+    },
+    "colorama": {
+        "package": "colorama"
+    },
+    "GDReq": {
+        "package": "GDReq",
+        "args": ["-i", "https://test.pypi.org/simple/"]
+    }
 }
 
 def installMissingModules(modules):
     installedSomething = False
-
-    for importName, pipName in modules.items():
+    for importName, moduleInfo in modules.items():
         try:
             importlib.import_module(importName)
+            
         except ImportError:
-            print(f"{pipName} is not installed. Installing...")
-            subprocess.check_call([sys.executable, "-m", "pip", "install", pipName])
+            packageName = moduleInfo["package"]
+            extraArgs = moduleInfo.get("args", [])
+            print(f"{packageName} is not installed. Installing...")
+            subprocess.check_call([
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                *extraArgs,
+                packageName])
             installedSomething = True
-
     if installedSomething:
         subprocess.check_call([sys.executable] + sys.argv)
         sys.exit()
